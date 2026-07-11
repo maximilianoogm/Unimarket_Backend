ef7e390a3bfd9eadad14f8bfd1dc6e18e8c6c769
Valentino Mantilla <vmc.280805@gmail.com>
2026-07-11 11:41:23 -0500

    feat: infraestructura cloud completada, conexión a Render y seed validados

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,14 @@
 import "dotenv/config";
+import process from "process"; // <-- Añadimos esta importación nativa de Node.js
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    // Inyectamos el comando nativo para ejecutar tu script de Node.js en Prisma 7
     seed: "node ./prisma/seed.js", 
   },
   datasource: {
-    url: "postgresql://unimarket:unimarket@localhost:5432/unimarketdb",
+    url: process.env.DATABASE_URL, 
   },
 });
